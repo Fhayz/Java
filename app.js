4eb9d80bc3e14d16637a26e4204e9ab7d1da4fd6
Favour <Favourdozie@gmail.com>
2023-02-17 14:59:03 +0100

    Second commit

---
+++ app.js
@@ -0,0 +1,45 @@
+let varArr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    7, 8, 9,
+    [[10,11,12,],[13,4,112]],
+    [136,14]
+  ];
+  console.log(varArr[5][0][0]);
+  console.log(varArr[3]);
+  console.log(varArr[5][1][0]);
+  console.log(varArr[6][0]);
+  console.log(varArr[1][0]);
+  // document.write(varArr[5][0][0]); 
+  // document.write(varArr[3]);
+  // document.write(varArr[5][1][0]);
+  // document.write(varArr[6][0]);
+  // document.write(varArr[1][0]);
+
+  //question 2//
+  let varSentence = ["javascript", "web", "CapacityBay"];
+  console.log('I am a ' + '' + varSentence[1] + ' at '+ varSentence[2] + ' and I love '+varSentence[0] )
+ 
+  //question 3//
+
+  let fish = ['Generalization'] 
+  console.log(fish[0][3])
+  console.log(fish[0][4])
+  console.log(fish[0][5])
+  console.log(fish[0][6])
+  console.log(fish[0][7])
+  console.log(fish[0][8])
+  console.log(fish[0][9])
+  console.log(fish[0][10])
+  console.log(fish[0][11])
+  console.log(fish[0][12])
+  console.log(fish[0][13])
+
+  //question 4//
+  let list = ["Orange", "apple", "guava", "Lemon"];
+  console.log(list);
+  list.push("peas", "banana")
+  console.log(list)
+  list.push("Avocado", "Mango");
+  console.log(list);
+  // document.write(list)
